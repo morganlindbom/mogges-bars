68e10e16f40ed78c269bb736397af52871277954
Morgan Lindbom <din-email@example.com>
2026-04-19 00:24:43 +0200

    feat: implement basic Home page and connect App component

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+// filename: src/pages/Home.jsx
+
+/**
+ * Home page
+ *
+ * Main page of the application.
+ */
+function Home() {
+  return (
+    <div>
+      <h1>My Bars</h1>
+    </div>
+  );
+}
+
+export default Home;
